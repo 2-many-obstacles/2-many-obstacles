dfb24c5124e753fad7e7624c54055d72d4a19b2e
Simon Knott <info@simonknott.de>
2025-03-21 17:43:19 +0100

    fullscreen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,17 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function App() {
   return (
-    <Map
-      // https://visgl.github.io/react-map-gl/docs/get-started/mapbox-tokens
-      mapboxAccessToken="pk.eyJ1Ijoic2tuMHR0IiwiYSI6ImNrd25lM2prMjI1MGgyd21kbDRuOTRib24ifQ.JLDxqFK3HC9rKzQIBCxMWg"
-      initialViewState={{
-        longitude: -100,
-        latitude: 40,
-        zoom: 3.5
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <div className='w-full h-screen'>
+      <Map
+        mapboxAccessToken="pk.eyJ1Ijoic2tuMHR0IiwiYSI6ImNrd25lM2prMjI1MGgyd21kbDRuOTRib24ifQ.JLDxqFK3HC9rKzQIBCxMWg"
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5
+        }}
+        style={{width: "100%", height: "100%"}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </div>
   );
 }
