0404c65241f6572cd3143b094daff989bc14b1ba
luisegarberding <luise.garberding@gmx.de>
2025-03-21 18:54:28 +0100

    new fodler

---
+++ src/pages/settings/index.ts
@@ -0,0 +1,20 @@
+/*
+
+Rollstuhlfahrer
+- Routenpräferez: empfohlen
+
+Profilparameter:
+- Maximale Steigung: default 6 (3/6/10/15)
+- Maximale Bordsteinhöhe: 0.06 (0,03/0,06/0,1)
+- Minimale Wegbreite: 1 (0-30) m
+- Oberflächenbeschaffenheit: Gut (slider mit Farben)
+- Min. Oberflächentyp
+- Min. Wegzustand
+- Features vermin: Stufen/Furten
+
+- Features vermeiden: Stufen/Furten/Fähren default: Stufen + Furten
+- Grenzen vermeiden: leer
+- Länder vermeiden: leer
+- Alternativ Routen: 1
+
+*/
