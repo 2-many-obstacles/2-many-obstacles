c55d3b6b94136af944d8c124fd7eaebab2bce9a4
luisegarberding <luise.garberding@gmx.de>
2025-03-21 21:06:08 +0100

    max curbs

--- src/pages/settings/max_curb.tsx
+++ src/pages/settings/max_curb.tsx
@@ -1,36 +1,38 @@
+import { useState } from "react";
+
 export default function MaxSlope(props: { onSubmit: (curb: string, next: boolean) => void }) {
-  const slopeOptions = [
+  const [curb, setCurb] = useState<string>("6 cm")
+  const curbOptions = [
     { value: "3 cm"},
     { value: "6 cm"},
     { value: "10 cm"},
     ];
 
-  const handleChange = (curb: string, next: boolean) => {
-    props.onSubmit(curb, next);
-  };
 
   return (
     <div className="p-4">
       <h2 className="text-lg font-bold">Max Curb Height</h2>
       <div className="mt-4">
-        <p className="mb-2 text-sm font-medium">Select Maximum Slope in your route:</p>
-        {slopeOptions.map((option) => (
+        <p className="mb-2 text-sm font-medium">Select Maximum Curb Height in your route:</p>
+        {curbOptions.map((option) => (
           <label key={option.value} className="block mb-2">
             <input
               type="radio"
               name="slope"
               value={option.value}
-              onChange={() => handleChange(option.value, true)}
+              checked={curb === option.value}
+              onChange={() => setCurb(option.value)}
               className="mr-2"
             />
+            {option.value}
          </label>
         ))}
       </div>
         <div className="mt-4 flex justify-between">
-            <button className="p-2 bg-red-500 text-white rounded" onClick={() => props.onSubmit(option.value, false)}>
+            <button className="p-2 bg-red-500 text-white rounded" onClick={() => props.onSubmit(curb, false)}>
             Back
             </button>
-            <button className="p-2 bg-blue-500 text-white rounded" onClick={() => props.onSubmit("300cm",true)}>
+            <button className="p-2 bg-blue-500 text-white rounded" onClick={() => props.onSubmit(curb,true)}>
             Next
             </button>
         </div>
