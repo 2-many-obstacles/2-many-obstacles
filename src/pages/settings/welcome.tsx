322597a3fda7f57963802fd460160c410476a046
Simon Knott <info@simonknott.de>
2025-03-22 11:03:32 +0100

    typo

--- src/pages/settings/welcome.tsx
+++ src/pages/settings/welcome.tsx
@@ -4,7 +4,7 @@ export default function Welcome(props : {onSubmit : (advancedMobility: boolean) 
   return (
     <div className="p-4">
       <h2 className="text-lg font-bold">Welcome!</h2>
-      <h3 className="text-sm mt-2">Do you want to costumize advanced mobility settings?</h3>
+      <h3 className="text-sm mt-2">Do you want to customize advanced mobility settings?</h3>
         <div className="mt-4 flex justify-between">
         <div className="fixed bottom-4 left-0 right-0 flex justify-center gap-16 px-4">
         <button className="p-2 bg-red-500 text-white rounded" onClick={() => props.onSubmit(false)}>
